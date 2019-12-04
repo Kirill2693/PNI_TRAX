b233c8ed7f1e168acfc7fcdc0ae6a37240d1a6c4
Kirill2693 <58084318+Kirill2693@users.noreply.github.com>
2019-12-04 13:48:04 +0300

    Update main.cpp

--- PNI/main.cpp
+++ PNI/main.cpp
@@ -6,14 +6,9 @@ using namespace std;
 int main()
 {
     pars p;
-    p.setB("ugfjfubF125jhbf09F2000935");
+    p.setB("0541cc000018429c999a");//05_41cc0000_18_429c999a
     string str ="F2";
-   // int i = p.HEX_to_INT(str);
-   // cout<<i<<endl;
-    //cout<<"searc id-----------------------"<<endl;
-    //int position = p.serch_ID(241);
-    //cout<<position<<endl;
-    //bool dt = p.check_data(position,8);
     p.pars_data(0);
+    p.print_data();
     return 0;
 }
