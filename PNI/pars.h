56e6e4fa3768c6c9c83f7a87fd8478b94d3c99cf
Kirill2693 <58084318+Kirill2693@users.noreply.github.com>
2019-12-04 03:20:08 +0300

    Update pars.h

--- PNI/pars.h
+++ PNI/pars.h
@@ -17,7 +17,8 @@ public:
       int id;
       int size;
       bool completed;
-      string type;
+      int type;
+      string name;
     };
 
     void setB(std::string str)
@@ -40,23 +41,23 @@ private:
 std::string buffer;
 int size = 17;
 component pac[17] = {
-    {"0", 5, 8,false,"float"},
-    {"0",24,8,false,"float"},
-    {"0",25,8,false,"float"},
-    {"0",79,2,false,"char"},
-    {"0",77,32,false,"float4"},
-    {"0",7,8,false,"float"},
-    {"0",8,2,false,"bool"},
-    {"0",9,2,false,"bool"},
-    {"0",21,8,false,"float"},
-    {"0",22,8,false,"float"},
-    {"0",23,8,false,"float"},
-    {"0",27,8,false,"float"},
-    {"0",28,8,false,"float"},
-    {"0",29,8,false,"float"},
-    {"0",74,8,false,"float"},
-    {"0",75,8,false,"float"},
-    {"0",76,8,false,"float"}
+    {"0", 5, 8,false,1,"kHeading"},
+    {"0",24,8,false,1,"kPitch"},
+    {"0",25,8,false,1,"kRoll"},
+    {"0",79,2,false,2,"kHeadingStatus"},
+    {"0",77,32,false,3,"kQuaternion"},
+    {"0",7,8,false,1,"kTemperature"},
+    {"0",8,2,false,4,"kDistortion"},
+    {"0",9,2,false,4,"kCalStatus"},
+    {"0",21,8,false,1,"kAccelX"},
+    {"0",22,8,false,1,"kAccelY"},
+    {"0",23,8,false,1,"kAccelZ"},
+    {"0",27,8,false,1,"kMagX"},
+    {"0",28,8,false,1,"kMagY"},
+    {"0",29,8,false,1,"kMagZ"},
+    {"0",74,8,false,1,"kGyroX"},
+    {"0",75,8,false,1,"kGyroY"},
+    {"0",76,8,false,1,"kGyroZ"}
  };
 };
 
